8dc05bc7b053836c2024202dc6159403e1d1a68e
Alireza Rezapour <hamtapay.rezapour@gmail.com>
2025-10-11 14:21:19 +0330

    feat: enhance Blockchain struct with transaction pool and implement basic methods

--- internal/blockchain/blockchain.go
+++ internal/blockchain/blockchain.go
@@ -7,28 +7,40 @@ import (
 )
 
 type Blockchain struct {
-	config domain.BlockchainConfig
+	config  domain.BlockchainConfig
+	memPool *TransactionPool
+}
+
+func NewBlockchain(config domain.BlockchainConfig) *Blockchain {
+	return &Blockchain{config: config, memPool: NewTransactionPool()}
 }
 
 func (bc *Blockchain) AddBlock(ctx *context.Context, block *domain.Block) error {
-}
-
-func (bc *Blockchain) ValidateBlock(ctx *context.Context, b *domain.Block) error {
+	return nil
 }
 
 func (bc *Blockchain) AddTransaction(ctx *context.Context, tx *domain.Transaction) error {
+	return bc.memPool.AddTransaction(tx)
+}
+
+func (bc *Blockchain) ValidateBlock(ctx *context.Context, b *domain.Block) error {
+	return nil
 }
 
 func (bc *Blockchain) GetLatestBlock() *domain.Block {
+	return nil
 }
 
 func (bc *Blockchain) GetMempool() []*domain.Transaction {
+	return nil
 }
 
 func (bc *Blockchain) GetChain() []*domain.Block {
+	return nil
 }
 
 func (bc *Blockchain) ValidateChain() error {
+	return nil
 }
 
 var _ domain.IBlockchain = (*Blockchain)(nil)
